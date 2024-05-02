926731e73c16d316cf1af7c6fed54a893016ab37
Vinicius de Carvalho <sistemas@macromaq.com.br>
2024-05-02 18:12:05 -0300

    primeira versao

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita o envio do formulário padrão
+  
+    // Aqui você pode adicionar a lógica para enviar o formulário
+    alert('Formulário enviado com sucesso!');
+  });
+  
