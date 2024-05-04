cba7323c53fe00c88ddb8e1a00e6a098a9b34201
Vinicius de Carvalho <vinicius_carvalho@hotmail.com>
2024-05-03 21:16:18 -0300

    atualizacao Landing

--- index.js
+++ index.js
@@ -9,20 +9,3 @@ var prevScrollpos = window.scrollY;
       }
       prevScrollpos = currentScrollPos;
     }
-// function initMap() {
-//     // Coordenadas da localização (latitude e longitude)
-//     var location = { lat: -23.550520, lng: -46.633308 }; // Exemplo: São Paulo, Brasil
-    
-//     // Criar um mapa com as coordenadas definidas
-//     var map = new google.maps.Map(document.getElementById('mapa'), {
-//       center: location,
-//       zoom: 15 // Nível de zoom
-//     });
-    
-//     // Marcar a localização no mapa
-//     var marker = new google.maps.Marker({
-//       position: location,
-//       map: map,
-//       title: 'Localização da Empresa'
-//     });
-//   }
