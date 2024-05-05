0701dce4bd57abbf462cab81ef85937a65eb6d29
Vinicius de Carvalho <vinicius_carvalho@hotmail.com>
2024-05-05 18:36:44 -0300

    Atu

--- index.js
+++ index.js
@@ -9,3 +9,21 @@ var prevScrollpos = window.scrollY;
       }
       prevScrollpos = currentScrollPos;
     }
+    window.addEventListener('resize', function () {
+      //var altura = window.innerHeight;
+      var largura = window.innerWidth;
+     
+      if (largura > 768) {
+        //Desktop
+        var eleSobreMob = document.querySelector('.sobre-mob')
+        eleSobreMob.className = 'sobre-mob-hidden';
+        var eleSobreDesk = document.querySelector('.sobre-desk-hidden')
+        eleSobreDesk.className = 'sobre-desk';
+      } else {
+        //Mobile
+        var eleSobreMob = document.querySelector('.sobre-mob-hidden')
+        eleSobreMob.className = 'sobre-mob';
+        var eleSobreDesk = document.querySelector('.sobre-desk')
+        eleSobreDesk.className = 'sobre-desk-hidden';
+      }  
+  });
